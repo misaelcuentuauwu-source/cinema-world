4f999277a4e2dfb24064023a1e7507682389de2e
misalchichaxd <misaelcuentuatv@gmail.com>
2025-08-05 21:24:22 -0600

    actualización de misa

--- ProyectoRedes.java
+++ ProyectoRedes.java
@@ -3,7 +3,7 @@ public class ProyectoRedes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String usuario = "eduardo";
-        String contrasena = "hola mundo";
+        String contrasena = "123456";
         System.out.println("ingrese el usuario");
         String Ausuario = sc.nextLine();
         System.out.println("ingrese la contrasena");
